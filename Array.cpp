9ff9209ac5c438d4990e2b4e432645380dc93b16
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2023-05-05 18:47:46 +0530

    Solved GFG problem

--- Array.cpp
+++ Array.cpp
@@ -42,3 +42,22 @@ public:
         }
     }
 };
+
+// Problem : Remove duplicate elements from sorted Array in O(1) space
+class Solution
+{
+public:
+    int remove_duplicate(int a[], int n)
+    {
+        int i = 0;
+        for (int j = 1; j < n; j++)
+        {
+            if (a[j] != a[i])
+            {
+                i++;
+                a[i] = a[j];
+            }
+        }
+        return i + 1;
+    }
+};
