f5e3a6ae1c6535e189a576260a665dd852b9e4e5
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2023-05-18 13:32:29 +0530

    Solved GFG problem

---
+++ SlidingWindow.cpp
@@ -0,0 +1,43 @@
+// Problem : Smallest window containing 0, 1 and 2 GFG
+class Solution
+{
+public:
+    // Time Complexity : O(N)
+    // Space Complexity : O(1)
+    int smallestSubstring(string S)
+    {
+        int n = S.size();
+        int i = 0;
+        int j = 0;
+        // ek vector le lenge 3 size ka jisme count karlenge 0,1,2 ki values ko
+        vector<int> res(3, 0);
+        int minWin = INT_MAX;
+        while (j < n)
+        {
+            // extract kar rahe hai number ko string mein se
+            int num = int(S[j]) - 48;
+            // increment kar rahe hai current ki frequency ko
+            res[num]++;
+            // agar 0,1,2 ki frequencies greater than 0 hai toh
+            // hum second pointer variable ko increment karenge and har baar minimum le lenge
+            // second pointer variable par jo number hai uski frequency decrease kardenge
+            if (res[0] > 0 && res[1] > 0 && res[2] > 0)
+            {
+                while (i < j && (res[0] > 0 && res[1] > 0 && res[2] > 0))
+                {
+                    minWin = min(minWin, j - i + 1);
+                    num = int(S[i]) - 48;
+                    res[num]--;
+                    i++;
+                }
+            }
+            j++;
+        }
+        // agar kabhi bhi 0,1,2 hai hi nahi aapke kisi ek window mein toh answer -1 hoga
+        if (minWin == INT_MAX)
+        {
+            return -1;
+        }
+        return minWin;
+    }
+};
