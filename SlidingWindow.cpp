6846c2b84e36d9f958f337bac9919c55058bd526
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2023-06-04 17:53:24 +0530

    Solved GFG Problem

--- SlidingWindow.cpp
+++ SlidingWindow.cpp
@@ -77,3 +77,46 @@ public:
         return count;
     }
 };
+
+// Problem : Count the number of subarrays having sum between L to R GFG
+class Solution
+{
+    // TC => O(2N)   SC => O(1)
+    // Hume woh subarray chahiye jinke elments ka sum between L and R ho toh hum ek kaam kar sakte hai ki un subarrays ko count karle
+    // jinka sum L se less ho and un subarrays ko separately count karle jinka sum less than equal to R ho and phir dono ko subtract karle
+    // jisse hum woh subarrays mil jaayenge jinka sum between L and R hoga
+public:
+    // Simply sliding winfow ka concept use kar rahe hai agar sum>target ho jaata hai toh sum ko decrease karne ki try karenge warna har baar
+    // current subarray mein kitne subarray ban rahe hai ye count karlenge
+    long long helper(vector<int> A, long long k, long long N)
+    {
+        int i = 0;
+        int j = 0;
+        long long count = 0;
+        long long sum = 0;
+        while (j < N)
+        {
+            sum += (long long)A[j];
+            if (sum > k)
+            {
+                while (sum > k)
+                {
+                    sum -= (long long)A[i];
+                    i++;
+                }
+            }
+            count += (j - i + 1);
+            j++;
+        }
+        return count;
+    }
+    long long countSubarray(int N, vector<int> A, long long L, long long R)
+    {
+        // Hum un subarrays ko count kar rahe hai jinka sum less than L ho because isse hum exclude kar paayenge inhe apne final answer mein
+        long long maxSumL = helper(A, L - 1, N);
+        // Hum un subarrays ko count kar rahe hai jinka sum less than equal to R ho taaki inme mein se un subarrays ko remove kar sake jinka
+        // sum less than L hai taaki hume woh subarrays mil jaayen jinka sum L to R mein hai
+        long long maxSumR = helper(A, R, N);
+        return abs(maxSumR - maxSumL);
+    }
+};
