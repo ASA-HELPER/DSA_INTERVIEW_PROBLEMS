1e1b477e8f412a8b1a3a038470b45d0ecde3ac23
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2023-06-16 12:47:11 +0530

    Solved LeetCode Problem

--- Tree.cpp
+++ Tree.cpp
@@ -26,3 +26,38 @@ public:
         return min;
     }
 };
+
+// Problem : Binary Search Tree Iterator Leetcode
+class BSTIterator
+{
+    // TC => O(1) for next and hasNext functions
+    // SC => O(height of BST)
+public:
+    // Saare nodes ko store karlenge hum using DFS (inorder traversal)
+    int curr = 0, pos = 0, store[10001];
+    void dfs(TreeNode *root)
+    {
+        if (!root)
+            return;
+        dfs(root->left);
+        store[pos++] = root->val;
+        dfs(root->right);
+    }
+    BSTIterator(TreeNode *root)
+    {
+        dfs(root);
+    }
+
+    // Ye current node ko return karega and next right pointer par jaayenge
+    int next()
+    {
+        return store[curr++];
+    }
+
+    // Ye batayega ki current node ke paas next right pointer hai ya nahi
+    bool hasNext()
+    {
+        // Ye pos variable right most node ka index hai BST mein
+        return curr < pos;
+    }
+};
