5a1fc4df976d4c18de93d0add4deafb5228229fe
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2023-08-24 19:54:05 +0530

    Solved GFG question

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,14 +1,50 @@
+// Problem : Delete node in Doubly Linked List GFG
+class Solution
+{
+    // TC => O(N)       SC => O(1)
+public:
+    Node *deleteNode(Node *head, int x)
+    {
+        Node *temp = head;
+        // Jab tak exact position par nahi paunch jaate tab tak temp pointer ko next node par lekar jaate jaayenge
+        while (--x)
+        {
+            temp = temp->next;
+        }
+        // jis node ko delete karna hai agar uska previous hai toh uske previous ke next mein node ka next daaldo
+        if (temp->prev)
+        {
+            temp->prev->next = temp->next;
+        }
+        // jis node ko delete karna hai agar uska next hai toh uske next ke previous mein node ka previous daaldo
+        if (temp->next)
+        {
+            temp->next->prev = temp->prev;
+        }
+        // Jis node ko delete karna hai agar woh head hi hai toh head ko ek aage badha do
+        if (temp == head)
+        {
+            head = head->next;
+        }
+        // delete keyword ki help se node ko delete kardo
+        delete temp;
+        return head;
+    }
+};
+
+// Problem : Check If Circular Linked List GFG
 bool isCircular(Node *head)
 {
+    // TC => O(N)     SC => O(1)
     // simply head ke next se start karenge
-    Node*temp = head->next;
+    Node *temp = head->next;
     // agar circular linked list hogi toh dubara head par hi paunch jaayenge warna temp=NULL hojaayega
-    while(temp && temp!=head)
+    while (temp && temp != head)
     {
         temp = temp->next;
     }
     // agar dubara head par hi paunch gaye hai toh circular linked list hai
-    if(temp==head)
+    if (temp == head)
     {
         return true;
     }
