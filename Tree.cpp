86c4343ac11f2bdaef11a529af02a2d261a58ccc
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2023-11-13 11:49:57 +0530

    Solved GFG question

--- Tree.cpp
+++ Tree.cpp
@@ -147,6 +147,195 @@ public:
     }
 };
 
+// Problem : Diagonal Travesal of Binary Tree
+vector<int> diagonal(Node *root)
+{
+    vector<int> ans;
+    if (root == NULL)
+        return ans;
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node *curr = q.front();
+        q.pop();
+        while (curr != NULL)
+        {
+            ans.push_back(curr->data);
+            if (curr->left)
+                q.push(curr->left);
+            curr = curr->right;
+        }
+    }
+
+    return ans;
+}
+
+// Problem : Boundary Traversal of Binary Tree GFG
+// Question easy hai but thoda lengthy hai
+// Three parts mein karenge isse hum :
+// Part-1 : saare left non-leaf nodes ko lenge hum
+// Part-2 : saare leaf nodes ko collect karo
+// Part-3 : saare right nodes ko collect karo but in reverse order
+
+class Solution
+{
+public:
+    void traverseLeft(Node *root, vector<int> &ans)
+    {
+        if ((root == NULL) || (root->left == NULL && root->right == NULL))
+            return;
+
+        ans.push_back(root->data);
+
+        if (root->left)
+            traverseLeft(root->left, ans);
+        else
+            traverseLeft(root->right, ans);
+    }
+
+    void traverseLeaf(Node *root, vector<int> &ans)
+    {
+        if (root == NULL)
+            return;
+
+        if (root->left == NULL && root->right == NULL)
+        {
+            ans.push_back(root->data);
+            return;
+        }
+
+        traverseLeaf(root->left, ans);
+        traverseLeaf(root->right, ans);
+    }
+
+    void traverseRight(Node *root, vector<int> &ans)
+    {
+        if ((root == NULL) || (root->left == NULL && root->right == NULL))
+            return;
+
+        if (root->right)
+            traverseRight(root->right, ans);
+        else
+            traverseRight(root->left, ans);
+
+        ans.push_back(root->data);
+    }
+
+    vector<int> boundary(Node *root)
+    {
+        vector<int> ans;
+
+        if (root->left == NULL && root->right == NULL)
+        {
+            ans.push_back(root->data);
+            return ans;
+        }
+
+        ans.push_back(root->data);
+
+        traverseLeft(root->left, ans);
+
+        traverseLeaf(root->left, ans);
+        traverseLeaf(root->right, ans);
+
+        traverseRight(root->right, ans);
+
+        return ans;
+    }
+};
+
+// Problem : Top View of Binary tree GFG
+class Solution
+{
+public:
+    // Yahan par hum horizontal distance ka concept use karenge and level order traversal
+    // karenge
+    vector<int> topView(Node *root)
+    {
+        if (root == NULL)
+        {
+            return {};
+        }
+        vector<int> res;
+        queue<pair<Node *, int>> q;
+        map<int, int> mp;
+        q.push({root, 0});
+        mp[0] = root->data;
+        while (!q.empty())
+        {
+            int qsize = q.size();
+            int maxi = INT_MIN;
+            for (int i = 0; i < qsize; i++)
+            {
+                Node *curr = q.front().first;
+                int hd = q.front().second;
+                q.pop();
+                if (!mp[hd])
+                {
+                    mp[hd] = curr->data;
+                }
+                if (curr->left)
+                {
+                    q.push({curr->left, hd - 1});
+                }
+                if (curr->right)
+                {
+                    q.push({curr->right, hd + 1});
+                }
+            }
+        }
+        for (auto it : mp)
+        {
+            res.push_back(it.second);
+        }
+        return res;
+    }
+};
+
+// Problem : Bottom View of Binary Tree GFG
+class Solution
+{
+public:
+    vector<int> bottomView(Node *root)
+    {
+        if (root == NULL)
+        {
+            return {};
+        }
+        vector<int> res;
+        queue<pair<Node *, int>> q;
+        map<int, int> mp;
+        q.push({root, 0});
+        mp[0] = root->data;
+        while (!q.empty())
+        {
+            int qsize = q.size();
+            int maxi = INT_MIN;
+            for (int i = 0; i < qsize; i++)
+            {
+                Node *curr = q.front().first;
+                int hd = q.front().second;
+                q.pop();
+                mp[hd] = curr->data;
+                if (curr->left)
+                {
+                    q.push({curr->left, hd - 1});
+                }
+                if (curr->right)
+                {
+                    q.push({curr->right, hd + 1});
+                }
+            }
+        }
+        for (auto it : mp)
+        {
+            res.push_back(it.second);
+        }
+        return res;
+    }
+};
+
 // Problem : Print Nodes having K leaves GFG
 class Solution
 {
